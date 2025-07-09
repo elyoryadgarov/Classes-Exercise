93822ac912c220bb65b0af18ce3f5b96b18dc147
Elyor Yadgarov <eyodgorov@gmail.com>
2025-07-08 23:49:20 -0400

    Classes exersice

---
+++ Classes/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace Classes;
+
+public class Car
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Make { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            car.Make = "VW";
+            car.Year = 2005;
+            car.Model = "Jetta";
+            
+            Console.WriteLine(car.Make + " \n" + car.Year + " \n"+car.Model);
+            
+            Car Toyota = new Car()
+            {
+                Make = "Toyota", 
+                Year = 2018, 
+                Model = "Highlander"
+            };
+            Console.WriteLine(Toyota.Make +  " \n" + Toyota.Year + " \n"+Toyota.Model);
+            
         }
     }
 }
